bed5304023798abe29fbc692260f118a892d8e28
Hassan Amin <145911534+HassanAmin-67@users.noreply.github.com>
2024-09-10 09:27:14 -0700

    Add files via upload

---
+++ 6.sql
@@ -0,0 +1,17 @@
+-- The names of songs that are by Post Malone.
+SELECT name
+FROM songs
+WHERE artist_id =
+(
+    SELECT id
+    FROM artists
+    WHERE name = 'Post Malone'
+);
+
+/*
+-- The names of songs that are by Post Malone.
+SELECT songs.name
+FROM songs
+JOIN artists ON songs.artist_id = artists.id
+WHERE artists.name = 'Post Malone';
+*/
